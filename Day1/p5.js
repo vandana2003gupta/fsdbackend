c34964560aca3d2941bd40c314ed9f56cbd5babb
vandana2003gupta <vandana2003gupta@gmail.com>
2025-02-17 15:19:35 +0530

    Read and Write File

---
+++ Day1/p5.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const read=()=>{
+/fs.readFile("./data.txt","utf8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+
+})
+}
+console.log('Before Reading')
+read();
+console.log('After Reading')
---
+++ Day1/p6.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const write = () => {
+    /* const write =(data) => { */
+    const data = "This is my new file ";
+    /* fs.writeFile("./data.txt", "I am a new file", 'utf8', (err) => { */
+    fs.writeFile("./data.txt", data, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("File has been written successfully!");
+    }
+  });
+};
+
+console.log('Before Writing');
+write();
+console.log('After Writing');
